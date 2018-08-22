78a368e4bc4b0996205fabf7607989986b9698ee
Cogorlopez <Cogorlopez@gmail.com>
2018-08-22 13:55:42 -0500

    Login component work. Also some stylizing

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <footer className="text-black mt-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} Komatsu
+    </footer>
+  );
+};
